873d2e4758c60cd4c2e7144c3fed2b5adb8c9467
615046 <walter.615046@swl.k12.oh.us>
2025-09-08 08:55:25 -0400

    test

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,30 @@
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world - this is a test");
-    }
-}
+        //int Variables
+        int intOperandA = 7;
+        int intOperandB = 4;
+        int intSum;
+        int intDifference;
+        int intProduct;
+        int intQuotient;
+        int intModulo;
+        //double Variables
+        double doubleOperandA = 3;
+        double doubleOperandB = 6;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+        double doubleModulo;
+        // Statements
+        intSum= intOperandA + intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intProduct= intOperandA * intOperandB;
+        intQuotient = intOperandA /intOperandB;
+        intModulo = intOperandA %  intOperandB;
+        doubleSum= doubleOperandA+doubleOperandB;
+        doubleProduct= doubleOperandA*doubleOperandB;
+        doubleDifference=doubleOperandA-doubleOperandB;
+        doubleQuotient= doubleOperandA/doubleOperandB;
+        doubleModulo=doubleOperandA%doubleOperandB;
+        // Output Statements
