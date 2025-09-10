433f3eb174aa746adce677527f3ac8459a46b03f
615046 <walter.615046@swl.k12.oh.us>
2025-09-10 08:18:03 -0400

    send

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+
+Scanner scan= new Scanner(System.in);
+        //variables
+        double itemCost;
+        double SALES_TAX= .05;
+        double totalCost;
+
+        //Ask user the price of purchase
+        System.out.println("What is the price of your purchase?");
+            itemCost=scan.nextDouble();//
+        totalCost= itemCost+(itemCost*SALES_TAX);
+
+        //Give user total cost
+        System.out.println("Your total cost is $" + totalCost);
+    }
 }
