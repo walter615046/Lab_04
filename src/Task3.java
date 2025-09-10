1efd991ff4cfcc40ea9b6dec0bc477d065643ba9
615046 <walter.615046@swl.k12.oh.us>
2025-09-10 08:29:05 -0400

    send

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
+
 public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+            //Variables
+            double springCost;
+            double summerCost;
+            double fallCost;
+            double winterCost;
+            double totalCost;
+
+            //Ask the cost for each season
+        System.out.println("What is the maintenance cost for the spring?");
+            springCost=scan.nextDouble();
+        System.out.println("What is the maintenance cost for the summer?");
+            summerCost=scan.nextDouble();
+        System.out.println("What is the maintenance cost for the fall?");
+            fallCost=scan.nextDouble();
+        System.out.println("What is the maintenance cost for the winter?");
+            winterCost=scan.nextDouble();
+
+            //Calculate total cost
+        totalCost= springCost + summerCost + fallCost + winterCost;
+
+            //Give total Cost
+        System.out.println("The total maintenance year round is $" + totalCost);
+
+    }
 }
