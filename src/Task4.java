df91d4b8f0b3ae2e1538f72879ea9df541bcbe6d
615046 <walter.615046@swl.k12.oh.us>
2025-09-10 08:55:06 -0400

    send

--- src/Task4.java
+++ src/Task4.java
@@ -1,4 +1,18 @@
 public class Task4 {
 public static void main(String[] args) {
+
+        //Variables
+        double ORIGINAL_BALANCE= 5000;
+        double INTEREST_RATE = .17;
+        double monthOneCost;
+        double monthTwoCost;
+
+        //Statements
+    monthOneCost = ORIGINAL_BALANCE+(ORIGINAL_BALANCE * INTEREST_RATE);
+    monthTwoCost = monthOneCost+ (monthOneCost * INTEREST_RATE);
+
+        //Output
+    System.out.println("Your credit card balance after one month is $" + monthOneCost);
+    System.out.println("Your credit card balance after two months is $" + monthTwoCost);
 }
 }
--- src/Task5.java
+++ src/Task5.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Task5 {
-}
+    public static void main(String[] args){
+
+Scanner scan= new Scanner(System.in);
+        //variables
+        double degreeFahrenheit;
+        double degreeCelsius;
+
+        //Ask the temp in F
+        System.out.println("What is the temperature in Fahrenheit");
+            degreeFahrenheit=scan.nextDouble();
+        //Statement Conversion
+        degreeCelsius= (degreeFahrenheit-32) * .555555555;
+
+        //Output Celsius
+        System.out.println("The temperature in Celsius is " + degreeCelsius);
+
+
+
+
+    }
+        }
